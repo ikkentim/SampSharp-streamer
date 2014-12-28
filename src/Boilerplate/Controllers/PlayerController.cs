9f9b5d3a94c289057ba9971c4c21791809029b85
Tim Potze <timpotze@gmail.com>
2014-12-28 12:10:41 +0100

    Initial commit

---
+++ src/Boilerplate/Controllers/PlayerController.cs
@@ -0,0 +1,13 @@
+﻿using Boilerplate.World;
+using SampSharp.GameMode.Controllers;
+
+namespace Boilerplate.Controllers
+{
+    public class PlayerController : GtaPlayerController
+    {
+        public override void RegisterTypes()
+        {
+            Player.Register<Player>();
+        }
+    }
+}
---
+++ src/Boilerplate/World/Player.cs
@@ -0,0 +1,11 @@
+﻿using SampSharp.GameMode.World;
+
+namespace Boilerplate.World
+{
+    public class Player : GtaPlayer
+    {
+        public Player(int id) : base(id)
+        {
+        }
+    }
+}
