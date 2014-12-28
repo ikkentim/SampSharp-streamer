b34a7840997c48e2302e2719256d6a8d348b72c0
Tim Potze <timpotze@gmail.com>
2014-12-28 13:34:07 +0100

    Initial streamer commit

---
+++ src/TestMode/GameMode.cs
@@ -0,0 +1,59 @@
+﻿// SampSharp
+// Copyright (C) 2014 Tim Potze
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// 
+// For more information, please refer to <http://unlicense.org>
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using SampSharp.GameMode;
+using SampSharp.GameMode.Controllers;
+using SampSharp.GameMode.World;
+using TestMode.Tests;
+
+namespace TestMode
+{
+    public class GameMode : BaseMode
+    {
+        private readonly List<ITest> _tests = new List<ITest>
+        {
+            new StreamerTest(),
+        };
+
+        public override bool OnGameModeInit()
+        {
+            SetGameModeText("sa-mp# testmode");
+            UsePlayerPedAnims();
+
+            Debug.WriteLine("Loading player classes...");
+            AddPlayerClass(65, new Vector(5), 0);
+
+            foreach (ITest test in _tests)
+            {
+                Console.WriteLine("=========");
+                Console.WriteLine("Starting test: {0}", test);
+                test.Start(this);
+                Console.WriteLine();
+            }
+
+            return true;
+        }
+
+        protected override void LoadControllers(ControllerCollection controllers)
+        {
+            base.LoadControllers(controllers);
+
+            foreach (IControllerTest test in _tests.OfType<IControllerTest>())
+                test.LoadControllers(controllers);
+        }
+    }
+}
---
+++ src/TestMode/Properties/AssemblyInfo.cs
@@ -0,0 +1,54 @@
+﻿// SampSharp
+// Copyright (C) 2014 Tim Potze
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+// OTHER DEALINGS IN THE SOFTWARE.
+// 
+// For more information, please refer to <http://unlicense.org>
+
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("TestMode for SA-MP#")]
+[assembly: AssemblyDescription("TestMode for SA-MP#")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestMode for SA-MP#")]
+[assembly: AssemblyCopyright("Copyright © Tim Potze 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("745a1de2-d270-4bee-ba8c-f740601498dd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: NeutralResourcesLanguage("en")]
